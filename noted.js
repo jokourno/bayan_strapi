c94f00d7a67fc81b2596f29ef0195b8b0ca172a1
Joko Purnomo <jokourno12@gmail.com>
2023-10-17 08:58:49 +0700

    Noted

---
+++ noted.js
@@ -0,0 +1,30 @@
+/**
+ * pre-condition: install nodejs, install database yang ingin digunakan dan buat database-nya
+1. https://docs.strapi.io/dev-docs/quick-start
+2. npx create-strapi-app@4.3.9 <project>
+? Choose your installation type Custom (manual settings)
+? Choose your preferred language
+? Choose your preferred language JavaScript
+? Choose your default database client
+? Choose your default database client mysql                                                                                                
+? Database name: (bayapi) bayapidb
+? Database name: bayapidb
+? Host: (127.0.0.1) 
+? Host: 127.0.0.1
+? Port: (3306) 
+? Port: 3306
+? Username: <sesuai nama user login di database>
+? Username: <sesuai nama user login di database>
+? Password: <sesuai password masuk database dengan user tersebut>
+? Password: <sesuai password masuk database dengan user tersebut>
+? Enable SSL connection: (y/N) y
+? Enable SSL connection: Yes
+4. npm run develop
+5. npm run start
+6. npm run build
+7. npm run strapi
+8. npm run develop
+
+Jadi untuk menggunakan Strapi maka siapkan terlebih dahulu environment seperti database.
+Tinggal gunakan npx dan pilih database
+ */
